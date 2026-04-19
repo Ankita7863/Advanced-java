5e0981150017fa978c80b00f3d34fa1c7e47a567
Ankita7863 <gadavanteankitab148@gmail.com>
2026-04-19 20:22:27 +0530

    Add files via upload

---
+++ StringBuildingFunctions10.java
@@ -0,0 +1,17 @@
+package lab3b;
+
+public class StringBuildingFunctions10 {
+	// 10. Function to count words in a string
+	public static int countWords(String str) {
+	if (str == null || str.trim().isEmpty()) {
+	return 0;
+	}
+	return str.trim().split("\\s+").length;
+	}
+
+	public static void main(String[] args) {
+	System.out.println("10. Word Count: " + countWords("Hello world java"));
+	}
+	}
+
+}
