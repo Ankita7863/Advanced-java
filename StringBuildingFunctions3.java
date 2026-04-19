d9be40a0abaa665b21bb85200dfc993fd15dd257
Ankita7863 <gadavanteankitab148@gmail.com>
2026-04-19 20:19:02 +0530

    Add files via upload

---
+++ StringBuildingFunctions3.java
@@ -0,0 +1,17 @@
+package lab3b;
+
+public class StringBuildingFunctions3 {
+	// 3. Function to reverse a string
+	public static String reverseString(String str) {
+	if (str == null) {
+	return null;
+	}
+	return new StringBuilder(str).reverse().toString();
+	}
+
+	public static void main(String[] args) {
+	System.out.println("3. Reverse: " + reverseString("hello"));
+	}
+	}
+
+
