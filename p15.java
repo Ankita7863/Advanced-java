8977612064289f8d0251cb7ed74ac072f930d9bf
Ankita7863 <gadavanteankitab148@gmail.com>
2026-04-19 18:36:10 +0530

    Add files via upload

---
+++ p15.java
@@ -0,0 +1,27 @@
+package lab3b;
+
+
+import java.util.Scanner;
+
+public class p15 {
+
+    // User-defined function to remove whitespace
+    public static String removeWhitespace(String str) {
+        if (str == null) return null;
+
+        return str.replaceAll("\\s", "");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String result = removeWhitespace(input);
+
+        System.out.println("String after removing whitespace: " + result);
+
+        sc.close();
+    }
+}
