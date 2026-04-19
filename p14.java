4a2844b238913f3e2f83e4baa0e589f392df5ffe
Ankita7863 <gadavanteankitab148@gmail.com>
2026-04-19 19:01:05 +0530

    Update p14.java

--- p14.java
+++ p14.java
@@ -1,3 +1,6 @@
+//*Write a Java Program for Checking if a string reads the same backward as forward (ignoring case and punctuation) 
+//*using user defined function isPalindrome():
+
 package lab3b;
 import java.util.Scanner;
 
