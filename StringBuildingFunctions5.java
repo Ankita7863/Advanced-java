bc4762b3af736e187d68aa34bcfcce3edc31bfd3
Ankita7863 <gadavanteankitab148@gmail.com>
2026-04-19 20:20:43 +0530

    Add files via upload

---
+++ StringBuildingFunctions5.java
@@ -0,0 +1,14 @@
+package lab3b;
+
+public class StringBuildingFunctions5 {
+	// 5. Function to remove all whitespace from a string
+	public static String removeWhitespace(String str) {
+	return str == null ? null : str.replaceAll("\\s", "");
+	}
+
+	public static void main(String[] args) {
+	System.out.println("5. Remove Whitespace: " + removeWhitespace("Hello World"));
+	}
+	}
+
+
